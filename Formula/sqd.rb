9195fc1e5b02927530af6cffe58bb8677fc74a03
Nikita Klimin <nklimin007@gmail.com>
2022-09-21 20:14:23 +0300

    release: manual release to v0.7.1

--- Formula/sqd.rb
+++ Formula/sqd.rb
@@ -2,15 +2,15 @@ class Sqd < Formula
     desc "Subsquid - CLI"
     homepage "https://docs.subsquid.io/deploy-squid/squid-cli/"
     license "GNU"
-    version "0.7.0"
+    version "0.7.1"
 
     if OS.mac?
-        url "https://github.com/subsquid/subsquid-cli/releases/download/v0.7.0/subsquid-cli-0.7.0-macos-x64.tar.gz"
-        sha256 "73c9aea048efb6331b10a13a1d59cc9f3e9196a5a300a3c74ca59c5d0dac14d7"
+        url "https://github.com/subsquid/subsquid-cli/releases/download/v0.7.1/subsquid-cli-0.7.1-macos-x64.tar.gz"
+        sha256 "04fac50286b0a25c5c4b3dd0d500c8ebb378525126582066341ce81a130ade0c"
     end
     if OS.linux?
-        url "https://github.com/subsquid/subsquid-cli/releases/download/v0.7.0/subsquid-cli-0.7.0-linux-x64.tar.gz"
-        sha256 "02cf4dc6ea939998a6349ba5ae8dc7de9a85628fa1b28189325e7a3a63c113fd"
+        url "https://github.com/subsquid/subsquid-cli/releases/download/v0.7.1/subsquid-cli-0.7.1-linux-x64.tar.gz"
+        sha256 "3b44a054ca486e18cda3d0385c2531374434723a19f03600050c573002a2efb1"
     end
     
     def install
