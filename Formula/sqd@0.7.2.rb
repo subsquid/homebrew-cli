394f4d860160a21f3a6239d069feb3ff6204c2e9
Nikita Klimin <nklimin007@gmail.com>
2023-03-13 13:26:38 +0300

    test: separate 0.7.2 version

---
+++ Formula/sqd@0.7.2.rb
@@ -0,0 +1,19 @@
+class Sqd < Formula
+    desc "Subsquid - CLI"
+    homepage "https://docs.subsquid.io/deploy-squid/squid-cli/"
+    license "GNU"
+    version "0.7.2"
+
+    if OS.mac?
+        url "https://github.com/subsquid/subsquid-cli/releases/download/v0.7.2/subsquid-cli-0.7.2-macos-x64.tar.gz"
+        sha256 "22a03b698cdfc3732ed733ef2d420401ab61fee274d344e06776e4e194086ddb"
+    end
+    if OS.linux?
+        url "https://github.com/subsquid/subsquid-cli/releases/download/v0.7.2/subsquid-cli-0.7.2-linux-x64.tar.gz"
+        sha256 "598121cd0526c27b937f7985aaaa885c2ac8fb4397050d59040719ee6ecad100"
+    end
+    
+    def install
+        bin.install "sqd"
+    end
+end
